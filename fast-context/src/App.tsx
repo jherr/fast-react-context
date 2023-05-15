ce03947fff05434e932abd39da2b866be627b7fd
grigoriu-stefan-iulian <45462473+grigoriu-stefan-iulian@users.noreply.github.com>
2023-05-15 10:08:39 +0000

    add changes to right folder

--- fast-context/src/App.tsx
+++ fast-context/src/App.tsx
@@ -1,17 +1,89 @@
-import React from "react";
+import { useRef, createContext, useContext, useCallback, ReactNode } from "react";
+
+type Store = { first: string; last: string };
+
+const useStore = (): {
+  get: () => Store;
+  set: (value: Partial<Store>) => void;
+  subscribe: (callback: () => void) => void;
+} => {
+  const store = useRef({ 
+    first: '',
+    last: '',
+  });
+
+  const get = useCallback(() => store.current, []);
+  const set = useCallback((value: Partial<Store>) => {
+    store.current = { ...store.current, ...value };
+  }, []);
+
+  const subscribers = useRef(new Set<() => void>());
+
+  const subscribe = useCallback((callback: () => void) => {
+    subscribers.current.add(callback);
+
+    return () => subscribers.current.delete(callback);
+  }, []);
+
+
+  return {
+    get,
+    set,
+    subscribe,
+  };
+}
+
+const useMyContext = (): [
+  Store, 
+  (value: Partial<Store>) => void, 
+  (callback: () => void) => void,
+] => {
+  const store = useContext(MyContext);
+
+  if(!store) {
+    throw new Error('Store not found')
+  }
+
+  return [
+    store.get(),
+    store.set,
+    store.subscribe,
+  ];
+};
+
+type UseStoreDataReturnType = ReturnType<typeof useStore>;
+
+const MyContext = createContext<UseStoreDataReturnType| null>(null);
+
+const Provider = ({children}: {children: ReactNode}) => {
+  return (
+    <MyContext.Provider value={useStore()}>
+      {children}
+    </MyContext.Provider>
+  )
+};
 
 const TextInput = ({ value }: { value: "first" | "last" }) => {
+  const [ store, setStore ] = useMyContext()!
+
   return (
     <div className="field">
-      {value}: <input />
+      {store[value]}: (
+        <input 
+          value={store[value]} 
+          onChange={(e) => setStore({...store, [value]: e.target.value})}
+          />
+        )
     </div>
   );
 };
 
 const Display = ({ value }: { value: "first" | "last" }) => {
+  const [ store ] = useMyContext()!
+
   return (
     <div className="value">
-      {value}: {""}
+      {value}: store[value]
     </div>
   );
 };
@@ -48,10 +120,12 @@ const ContentContainer = () => {
 
 function App() {
   return (
-    <div className="container">
-      <h5>App</h5>
-      <ContentContainer />
-    </div>
+    <Provider>
+      <div className="container">
+        <h5>App</h5>
+        <ContentContainer />
+      </div>
+    </Provider>
   );
 }
 
