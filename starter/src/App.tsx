017555337e00e800ead357c23949b7c0f3df9d03
grigoriu-stefan-iulian <45462473+grigoriu-stefan-iulian@users.noreply.github.com>
2023-05-15 10:05:37 +0000

    add store usng useRef

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -1,23 +1,67 @@
-import { useState, createContext, useContext, memo } from "react";
+import { useRef, createContext, useContext, useCallback, ReactNode } from "react";
 
-const useStore = () => {
-  const state = useState({ 
+type Store = { first: string; last: string };
+
+const useStore = (): {
+  get: () => Store;
+  set: (value: Partial<Store>) => void;
+  subscribe: (callback: () => void) => void;
+} => {
+  const store = useRef({ 
     first: '',
     last: '',
   });
 
-  return state;
+  const get = useCallback(() => store.current, []);
+  const set = useCallback((value: Partial<Store>) => {
+    store.current = { ...store.current, ...value };
+  }, []);
+
+  const subscribers = useRef(new Set<() => void>());
+
+  const subscribe = useCallback((callback: () => void) => {
+    subscribers.current.add(callback);
+
+    return () => subscribers.current.delete(callback);
+  }, []);
+
+
+  return {
+    get,
+    set,
+    subscribe,
+  };
 }
 
-const useMyContext = () => {
+const useMyContext = (): [
+  Store, 
+  (value: Partial<Store>) => void, 
+  (callback: () => void) => void,
+] => {
   const store = useContext(MyContext);
 
-  return store;
+  if(!store) {
+    throw new Error('Store not found')
+  }
+
+  return [
+    store.get(),
+    store.set,
+    store.subscribe,
+  ];
 };
 
 type UseStoreDataReturnType = ReturnType<typeof useStore>;
 
-const MyContext = createContext< UseStoreDataReturnType| null>(null);
+const MyContext = createContext<UseStoreDataReturnType| null>(null);
+
+const Provider = ({children}: {children: ReactNode}) => {
+  return (
+    <MyContext.Provider value={useStore()}>
+      {children}
+    </MyContext.Provider>
+  )
+};
 
 const TextInput = ({ value }: { value: "first" | "last" }) => {
   const [ store, setStore ] = useMyContext()!
@@ -44,7 +88,7 @@ const Display = ({ value }: { value: "first" | "last" }) => {
   );
 };
 
-const FormContainer = memo(() => {
+const FormContainer = () => {
   return (
     <div className="container">
       <h5>FormContainer</h5>
@@ -52,9 +96,9 @@ const FormContainer = memo(() => {
       <TextInput value="last" />
     </div>
   );
-});
+};
 
-const DisplayContainer = memo(() => {
+const DisplayContainer = () => {
   return (
     <div className="container">
       <h5>DisplayContainer Test</h5>
@@ -62,9 +106,9 @@ const DisplayContainer = memo(() => {
       <Display value="last" />
     </div>
   );
-});
+};
 
-const ContentContainer = memo(() => {
+const ContentContainer = () => {
   return (
     <div className="container">
       <h5>ContentContainer</h5>
@@ -72,21 +116,16 @@ const ContentContainer = memo(() => {
       <DisplayContainer />
     </div>
   );
-});
+};
 
 function App() {
-  const state = useState({ 
-    first: '',
-    last: '',
-  });
-
   return (
-    <MyContext.Provider value={state}>
+    <Provider>
       <div className="container">
         <h5>App</h5>
         <ContentContainer />
       </div>
-    </MyContext.Provider>
+    </Provider>
   );
 }
 
